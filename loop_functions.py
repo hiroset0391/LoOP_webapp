4e61bcd9f413d29dc8709f929b09d69b6055f9b7
hiroset0391 <takashi.hirose@bosai.go.jp>
2022-03-28 14:16:32 +0900

    Update loop_functions.py

--- loop_functions.py
+++ loop_functions.py
@@ -10,34 +10,6 @@ except ImportError:
 
 
 
-class Utils:
-
-    @staticmethod
-    def emit_progress_bar(progress: str, index: int, total: int) -> str:
-        """
-        A progress bar that is continuously updated in Python's standard
-        out.
-        :param progress: a string printed to stdout that is updated and later
-        returned.
-        :param index: the current index of the iteration within the tracked
-        process.
-        :param total: the total length of the tracked process.
-        :return: progress string.
-        """
-
-        w, h = get_terminal_size()
-        sys.stdout.write("\r")
-        if total < w:
-            block_size = int(w / total)
-        else:
-            block_size = int(total / w)
-        if index % block_size == 0:
-            progress += "="
-        percent = index / total
-        sys.stdout.write("[ %s ] %.2f%%" % (progress, percent * 100))
-        sys.stdout.flush()
-        return progress
-
 
 class LocalOutlierProbability(object):
     """
